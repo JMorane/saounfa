83aaa08e2dd098473afc1c1d797c8e58d488559c
Thomas Petit-Jean <30775613+Thomzoy@users.noreply.github.com>
2024-05-06 15:49:47 +0000

    watercolor tile

--- js/map.js
+++ js/map.js
@@ -9,6 +9,59 @@ const MATHIS_ICON = L.icon({
     // iconAnchor: [50, 25],
 });
 
+function setSrcWithStatusCheck(tile, url, fallback_url) {
+    function setSrc(effective_url) {
+        tile.src = effective_url;
+    }
+
+    // Make the request to fetch the image content
+    fetch(url)
+        .then(function (response) {
+            if (!response.ok) {
+                // Handle non-OK responses
+                setSrc(fallback_url);
+            } else {
+                // Convert the image content to base64
+                return response.blob();
+            }
+        })
+        .then(function (blob) {
+            return new Promise(function (resolve, reject) {
+                var reader = new FileReader();
+                reader.onloadend = function () {
+                    resolve(reader.result);
+                };
+                reader.onerror = reject;
+                reader.readAsDataURL(blob);
+            });
+        })
+        .then(function (dataUrl) {
+            // Set the source of the image using the base64 data URL
+            setSrc(dataUrl);
+        })
+        .catch(function () {
+            // Handle network errors
+            setSrc(fallback_url);
+        });
+}
+
+class Fallback extends L.TileLayer {
+    constructor(urlTemplate, options, fallbackUrl) {
+        super(urlTemplate, options);
+        this.fallbackLayer = L.tileLayer(fallbackUrl, options);
+    }
+
+    createTile(coords, done) {
+        const tile = super.createTile(coords, done);
+        this.fallbackLayer._tileZoom = this._tileZoom;
+        tile._originalCoords = coords;
+
+        setSrcWithStatusCheck(tile, tile.src, this.fallbackLayer.getTileUrl(coords));
+
+        return tile;
+    }
+}
+
 class CurrentPosition {
     // Current position of the user
     // Stores the marker + the circle around it
@@ -66,9 +119,20 @@ export function initializeMap() {
     map.setZoom(100);
 
     // Add a tile layer to the map
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+    var default_tile = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var tile = "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg?api_key=3e5104ec-9859-4ec2-b391-1f9caf641112"
+
+    var theTile = new Fallback(tile, {
+    minZoom: 1,
+    maxZoom: 16,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}, default_tile);
+
+    //var tile = "http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg"
+    theTile.addTo(map);
+    // L.tileLayer(tile, {
+    //     attribution: '© OpenStreetMap contributors'
+    // }).addTo(map);
 }
 
 export function onLocationFound(e) {
