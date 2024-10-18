5d60cab087babd0f76abd678182526808cce1f34
Thomas Petit-Jean <30775613+Thomzoy@users.noreply.github.com>
2024-10-18 15:59:03 +0000

    working with localStorage

--- js/itinerary.js
+++ js/itinerary.js
@@ -17,93 +17,71 @@ export function setItinerary(itinerary_id){
 }
 
 function itineraryOne(){
-    var markerManager = new MarkerManager({add_markers_at_init:false});
+    var markerManager = new MarkerManager({add_markers_at_init:false, show_next_marker:true});
 
     markerManager.addMarker({
         latlng : [48.822252, 2.358653],
         title : "Naturenville",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/naturenville.mp3"
     })
 
     markerManager.addMarker({
         latlng : [48.824471, 2.357990],
         title : "Rue Auguste Perret",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/perret.mp3"
     })
 
     markerManager.addMarker({
-        latlng : [48.8671938,2.3959993],
+        latlng : [48.828471, 2.359990], //[48.8671938,2.3959993],
         title : "Mathis",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/bel_air.mp3"
     })
 
     markerManager.addMarker({
         latlng : [48.86243,2.3850897],
         title : "Pere Lachaise",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/notre_dame.mp3"
     })
     
     markerManager.addMarker({
         latlng : [48.8653431,2.3915813],
         title : "Square du Docteur JAcques-Joseph-Grandier",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.8706068,2.394018],
         title : "Square des Saint-Simoniens",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.8716096,2.3926461],
         title : "Square Emmi-Pickler",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.8705521,2.3909355],
         title : "Villa de l'Ermitage",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.8700596,2.3915865],
         title : "pavillon Carré de Baudouin. ",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.8714766,2.3889373],
         title : "Regard Saint Martin",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 
     markerManager.addMarker({
         latlng : [48.9135076,2.1913571],
         title : "église Notre Dame de la Croix. ",
-        color : "lightgray",
-        marker_type : "inactive",
         mp3: "./assets/audio/tour_eiffel.mp3",
     })
 }
--- js/markers.js
+++ js/markers.js
@@ -3,11 +3,21 @@ import { getMap, getCurrentPos } from './main.js';
 
 const ICON_SIZE = 30;
 
-function setStoredMarker(marker_id, was_seen, displayed, color) {
+function getColor(is_next, is_close) {
+    if (is_close){
+        return "green"
+    }
+    if (is_next){
+        return "blue"
+    }
+    return "lightgray"
+}
+
+function setStoredMarker(marker_id, was_seen, is_next, is_close) {
     var value = {
         was_seen: was_seen,
-        displayed: displayed,
-        color: color
+        is_next: is_next,
+        is_close: is_close
     };
     //console.log("Setting");
     //console.log(marker_id, value);
@@ -19,45 +29,56 @@ function setStoredMarker(marker_id, was_seen, displayed, color) {
 function getStoredMarker(marker_id) {
     var defaultValue = {
         was_seen: false,
-        displayed: false,
-        color: "lightgray"
+        is_next: false,
+        is_close: false
     };
     var value = JSON.parse(localStorage.getItem(marker_id.toString()));
-    console.log(value);
     //console.log("Getting");
     //console.log(marker_id, value);
     return value !== null ? value : defaultValue;
 }
 
 class MarkerManager {
-    constructor({add_markers_at_init}) {
+    constructor({add_markers_at_init, show_next_marker}) {
         this.map = getMap();
         this.markers = [];
         this.add_markers_at_init = add_markers_at_init;
+        this.show_next_marker = show_next_marker;
         setInterval(this.activateMarkersNearby.bind(this), 1000);
     }
 
-    addMarker({latlng, color="lightgray", title = "Marker", marker_type = "inactive", mp3 = null}) {
-        console.log(`Adding ${title}`);
+    addMarker({latlng, title = "Marker", mp3 = null}) {
 
-        var idx = this.markers.length + 1;
-        //localStorage.removeItem(idx.toString());
-        var stored = getStoredMarker(idx)
+        console.log(`Adding ${title}`);
+        var idx = this.markers.length;
+        var stored = getStoredMarker(idx);
+        console.log(idx, stored);
+
+        if ((idx > 0) && (stored.is_next)){
+            console.log("change next");
+            // Then we have a stored next marker, we remove it from marker 1
+            var icon = new L.AwesomeNumberMarkers({
+                number: 1, 
+                markerColor: "lightgray",
+            });
+            this.markers[0].setIcon(icon);
+        }
+
+        var color = getColor(stored.is_next, stored.is_close)
 
         var icon = new L.AwesomeNumberMarkers({
-            number: idx, 
-            markerColor: stored.color,
+            number: idx + 1, 
+            markerColor: color,
         });
 
         var marker = L.marker(latlng, {icon: icon});
         marker.title = title;
-        marker.marker_type = marker_type;
         marker.idx = idx;
         marker.mp3 = mp3;
 
-        marker.color = stored.color;
         marker.was_seen = stored.was_seen;
-        marker.displayed = stored.displayed;
+        marker.is_next = stored.is_next;
+        marker.is_close = stored.is_close;
 
         // add click event
         marker.on('click', function() {
@@ -65,11 +86,11 @@ class MarkerManager {
             var audio = document.getElementById('audioPlayer');
             if ((!audio.paused) && (audio.src.split('/').pop() == marker.mp3.split('/').pop())){
                 // We check if we are on the same marker
-                // audio.pause();
+                audio.pause();
                 // audio.currentTime = 0; // Reset audio to the beginning
             }
             else {
-                if (marker.marker_type == "active"){
+                if (marker.was_seen){
                     console.log("play");
                     audio.src = marker.mp3;
                     if (audio.paused) {
@@ -80,64 +101,69 @@ class MarkerManager {
           });
 
         // add to map 
-        if ((this.markers.length == 0) || (this.add_markers_at_init)){
+
+        if (this.add_markers_at_init){
             // First marker OR any marker but should be shown anyway
-            marker.displayed = true;
-            // marker.addTo(this.map);
+            marker.was_seen = true;
         }
 
         // add to markers list
         this.markers.push(marker);
-
-        if (marker.displayed == true){
+            console.log("Number of markers: ", this.markers.length)
+        if ((marker.was_seen == true) || marker.is_next || (marker.idx==0)){
             marker.addTo(this.map);
         }
 
         return marker;
     }
 
-    updateMarker({marker, marker_type = null}) {
-        if ((marker_type == "active") && (marker.marker_type != "active"))  {
-            // Check if previous markers were seen
-            if ((marker.idx > 1) && !(this.markers[marker.idx-2].was_seen)) {
-                return
-            }
-            if (!marker.displayed){
-                this.markers[marker.idx-1].displayed = true;
-                this.markers[marker.idx-1].addTo(this.map)
-            }
-            var color = "green";
-            marker.marker_type = marker_type;
-            var icon = new L.AwesomeNumberMarkers({
-                number: marker.idx, 
-                markerColor: color,
-            });
-            marker.setIcon(icon);
-            marker.color = color;
-
-            // If not already playing, play the audio:
-            marker.fireEvent('click');
-            marker.was_seen = true;
-
-            // If next marker exists and isn't displayed, do it:
-            if ((marker.idx < this.markers.length) && !(this.markers[marker.idx].displayed)){
-                //this.markers[marker.idx].displayed = true;
-                //this.markers[marker.idx].addTo(this.map)
-            }
+    markerIsNearPosition(marker) {
+        console.log(marker.idx, "is near");
+        marker.is_next = false;
+        marker.is_close = true;
+        marker.was_seen = true;
+        marker.color = getColor(marker.is_next, marker.is_close);
+        var icon = new L.AwesomeNumberMarkers({
+            number: marker.idx + 1, 
+            markerColor: marker.color,
+        });
+        marker.setIcon(icon);
+        if ((marker.idx < this.markers.length + 1) && !(this.markers[marker.idx + 1].was_seen)){
+            // next marker should now be shown
+            this.markerIsNext(this.markers[marker.idx + 1]);
         }
-        if ((marker_type == "inactive") && (marker.marker_type != "inactive")){
-            var color = (marker.was_seen) ? 'blue' : 'lightgray';
+        setStoredMarker(marker.idx, marker.was_seen, marker.is_next, marker.is_close);
+    }
 
-            marker.color = color;
-
-            marker.marker_type = marker_type;
-            var icon = new L.AwesomeNumberMarkers({
-                number: marker.idx, 
-                markerColor: color,
-            });
-            marker.setIcon(icon);
+    markerIsNext(marker) {
+        console.log("Is next: ", marker);
+        marker.color = getColor(marker.is_next, marker.is_close);
+        marker.is_next = true;
+        console
+        var icon = new L.AwesomeNumberMarkers({
+            number: marker.idx + 1, 
+            markerColor: marker.color,
+        });
+        marker.setIcon(icon);
+        if (marker.was_seen == false){
+            marker.addTo(this.map);
         }
-        setStoredMarker(marker.idx, marker.was_seen, marker.displayed, marker.color);
+        setStoredMarker(marker.idx, marker.was_seen, marker.is_next, marker.is_close);
+    }
+
+    markerIsNotNearPosition(marker) {
+        if (!(marker.was_seen)){
+            // Wasn't seen yet, we do nothing
+            return
+        }
+        marker.is_close = false;
+        marker.color = getColor(marker.is_next, marker.is_close)
+        var icon = new L.AwesomeNumberMarkers({
+            number: marker.idx + 1, 
+            markerColor: marker.color,
+        });
+        marker.setIcon(icon);
+        setStoredMarker(marker.idx, marker.was_seen, marker.is_next, marker.is_close);
     }
 
     removeAllMarkers() {
@@ -151,15 +177,9 @@ class MarkerManager {
             const currentPos = getCurrentPos();
             var isWithinCircle = currentPos.circle.getBounds().contains(marker.getLatLng());
             if (isWithinCircle) {
-                self.updateMarker({
-                    marker,
-                    marker_type : "active",
-                });
+                self.markerIsNearPosition(marker);
             } else {
-                self.updateMarker({
-                    marker, 
-                    marker_type : "inactive",
-                });
+                self.markerIsNotNearPosition(marker);
             }
         }
         this.markers.forEach(activateMarkerNearBy);
